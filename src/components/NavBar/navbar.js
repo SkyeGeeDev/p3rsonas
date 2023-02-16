02258259f0b228d5152a6fa7394f802554584d3d
Scott Bui <skyegeegaming@gmail.com>
2023-02-16 12:12:21 -0500

    Added the Navbar

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return(
+        <ul>
+            <img src="" alt="logo"/>
+            <li>Home</li>
+            <li>Communities</li>
+            <li>Collection</li>
+            <li>Search</li>
+            <li>Login</li>
+        </ul>
+    )
+}
+
+export default NavBar
